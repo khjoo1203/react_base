4df1729f4b1e79ccff48fbd768ceb800795212fb
khjoo1203 <khjoo1203@naver.com>
2022-08-17 16:35:54 +0900

    완성!

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,14 +13,14 @@ return (
 
 
 
-const TodoTemplate = ({children}) => {
-    return (
-        <div className="TodoTemplate">
-            <div className="app-title">일정 관리</div>
-            <div className="content">{children}</div>
-        </div>
-    );
-};
+// const TodoTemplate = ({children}) => {
+//     return (
+//         <div className="TodoTemplate">
+//             <div className="app-title">일정 관리</div>
+//             <div className="content">{children}</div>
+//         </div>
+//     );
+// };
 
 
 export default Header;
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,30 +1,13 @@
 import './style.css';
 
 
-const Todo = ({todos, onRemove}) => {
+const Todo = () => {
+    
+
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
-            ))}
-        </div>
+        <div></div>
     );
-};
-
-const TodoListItem = ({todo, onRemove}) => {
-    const {id, text, checked} = todo;
-    return (
-        <div className="TodoListItem">
-            <div className={('checkbox', {checked})}>
-                {checked? "v": ""}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>지우기</div>
-        </div>
-    )
 }
-
-
 
 
 export default Todo;
